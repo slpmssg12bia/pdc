e0af2dad3838daa979aa93ea6ae3e24448f7d001
slpmssg12bia <102541001+slpmssg12bia@users.noreply.github.com>
2022-12-26 13:59:44 -0500

    Update archive_s3.sh

--- archive_s3.sh
+++ archive_s3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 sync pecosdump/ s3://viquity-database-import-us-east-1/Jobs/pdc/pdc_archive/pecosdump-"$(date +%d-%m-%y-%H-%M)"/
+aws s3 sync pdcdump/ s3://viquity-database-import-us-east-1/Jobs/pdc/pdc_archive/pdcdump-"$(date +%d-%m-%y-%H-%M)"/
