b75685a043e7c95c9b0d2efe495e70a7e909c3f5
slpmssg12bia <102541001+slpmssg12bia@users.noreply.github.com>
2022-12-29 10:47:08 -0500

    Update dump_to_s3.sh

--- dump_to_s3.sh
+++ dump_to_s3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 sync pdcdump/ s3://viquity-database-import-us-east-1/Jobs/pdc/pdcdump-"$(date +%d-%m-%y-%H-%M)"/
+aws s3 sync pdcdump/ s3://viquity-database-import-us-east-1/Jobs/pdc/pdc_current_dump/pdcdump/
